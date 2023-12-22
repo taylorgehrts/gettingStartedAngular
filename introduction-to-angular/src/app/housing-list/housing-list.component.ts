6580aba238b335909aa4760bc53c623d7ad8c1ca
taylorgehrts <taylorgehrts@gmail.com>
2023-12-22 15:59:25 +0100

    search works

--- introduction-to-angular/src/app/housing-list/housing-list.component.ts
+++ introduction-to-angular/src/app/housing-list/housing-list.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-list',
   templateUrl: './housing-list.component.html',
-  styleUrls: ['./housing-list.component.css']
+  styleUrls: ['./housing-list.component.css'],
 })
 export class HousingListComponent implements OnInit {
+  @Input() locationList: HousingLocation[] = [];
+  results: HousingLocation[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  searchHousingLocations(searchText: string) {
+    if (!searchText) return;
+    this.results = this.locationList.filter((location) =>
+      location.city.toLowerCase().includes(searchText.toLowerCase())
+    );
   }
-
 }
